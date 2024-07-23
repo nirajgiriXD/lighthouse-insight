cdc596efde3b6611ff63aa2be690005fed103e7c
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-07-23 15:17:09 +0545

    feat: config husky

--- components/SinglePage/singlePageReport.js
+++ components/SinglePage/singlePageReport.js
@@ -1,10 +1,10 @@
 const SinglePageReport = ({ report }) => {
   return (
-    <div className="w-[100vw] h-[calc(100vh-3.5rem)] overflow-hidden">
+    <div className="w-[100vw] h-[calc(100vh-5.5rem)] mx-auto overflow-hidden">
       <iframe
         srcDoc={report}
         title="Lighthouse Report"
-        className="w-full h-full"
+        className="w-full h-full overflow-hidden"
       />
     </div>
   );
